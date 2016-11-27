6d12f94196ada3c5a74b80af171600ccf3bd61fa
GS05-44-MatsumotoDaisuke <da.mats1990@gmail.com>
2016-11-27 23:49:14 +0900

    画像投稿できるよう修正

--- matsumoto/node-test/routes/boards.js
+++ matsumoto/node-test/routes/boards.js
@@ -14,8 +14,8 @@ cloudinary.config({
 router.get('/:board_id', function(req, res, next){
     var boardId = req.params.board_id;
     var getBoardQuery = 'SELECT * FROM boards WHERE board_id = ' + boardId;
-    var getMessagesQuery = 'SELECT M.message, M.image_path, ifnull(U.user_name, \'名無し\') AS user_name, DATE_FORMAT(M.created_at, \'%Y年%m月%d日 %k時%i分%s秒\') AS created_at FROM messages M LEFT OUTER JOIN users U ON M.user_id = U.user_id WHERE M.board_id = ' + boardId + ' ORDER BY M.created_at ASC';
-    connection.query(getBoardQuery, function(err, board){
+    var getMessagesQuery = 'SELECT M.message, M.image_path, ifnull(U.user_name, \'名無し\') AS user_name, DATE_FORMAT(M.created_at, \'%Y年%m月%d日 %k時%i分%s秒\') AS created_at FROM messages M LEFT OUTER JOIN users U ON M.user_id = U.user_id WHERE M.board_id = ' + boardId + ' ORDER BY M.created_at ASC'; // 変更
+  connection.query(getBoardQuery, function(err, board) {
         connection.query(getMessagesQuery, function(err, messages){
             res.render('board',{
                 title: board[0].title,
